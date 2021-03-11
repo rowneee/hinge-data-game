a574d28f6ed65abf4d282e5f78ef0a43bdcf5625
rowneee <roni.shabo@gmail.com>
2021-03-11 13:46:08 -0500

    added react frontend

---
+++ frontend/src/matches.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Matches extends React.Component {
+
+  render() {
+      return (
+          <div className="Matches">
+          </div>
+      );
+    }
+  }
+
+  export default Matches;
